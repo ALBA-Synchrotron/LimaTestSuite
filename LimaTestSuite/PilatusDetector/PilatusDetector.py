d05f4bca4fbd84f464e5e4423ae972633ea0ab84
Gabriel Jover-Manas <gjover@cells.es>
2017-05-04 15:59:53 +0200

    Fix SpecificDetector import
    Add debug info

--- LimaTestSuite/PilatusDetector/PilatusDetector.py
+++ LimaTestSuite/PilatusDetector/PilatusDetector.py
@@ -1,40 +1,20 @@
 import time
 import logging
-from LimaTestSuite.LimaDetector import Detector
+from LimaTestSuite.LimaCCDDetector import SpecificDetector
 from Lima import Pilatus
 
 
-class PilatusDetector(Detector):
+class PilatusDetector(SpecificDetector):
     def __init__(self, host, port):
         super(PilatusDetector, self).__init__()
-
-        self._AcqDefaults = {'acqExpoTime': 1,
-                             'acqNbFrames': 10,
-                             'acqMode': 'Single',
-                             'accMaxExpoTime': 1,
-                             'concatNbFrames': 0,
-                             'triggerMode': 'Internal',
-                             'latencyTime': 0,
-                             # 'AUTO_EXPO_MODE': 0 # NOT ACCESSIBLE in Simulator (at least)!
-                             }
-
-        self._SavingDefaults = {'directory': './',
-                                'prefix': 'img_',
-                                'suffix': '.cbf',
-                                'nextNumber': 0,
-                                'fileFormat': 'CBF',
-                                'savingMode': 'AUTO_FRAME',
-                                'overwritePolicy': 'OVERWRITE',
-                                'framesPerFile': 1,
-                                'nbframes': 0
-                                }
 
         try:
             if not port:
                 self.cam = Pilatus.Camera()
             else:
                 self.cam = Pilatus.Camera(port)
-            time.sleep(2)
             self.hwint = Pilatus.Interface(self.cam)
+            self.logger.debug("CAM %s HWI %s" % (self.cam, self.hwint))
         except Exception, e:
             logging.error('Error initializing detector object: \n%s', e)
+
